600480313dd1ea8c83a5f4d6b122c7d7b59498c5
Carlos Us Santiago <cdus12@gmail.com>
2019-09-04 17:31:20 -0700

    updated path for services Api

--- src/services/tempApi.js
+++ src/services/tempApi.js
@@ -1,5 +1,5 @@
-export const getMonitor = () => {
-  return fetch('https://protected-forest-33315.herokuapp.com/')
+export const getMonitors = () => {
+  return fetch('https://protected-forest-33315.herokuapp.com/api/v1/monitors')
     .then(res => {
       if(!res.ok) throw 'Unable to get monitor';
 
