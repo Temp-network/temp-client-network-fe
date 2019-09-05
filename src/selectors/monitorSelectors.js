2d4b4e59372ca3c064ec85d195214e643aefc853
Carlos Us Santiago <cdus12@gmail.com>
2019-09-04 17:31:29 -0700

    created selectors

---
+++ src/selectors/monitorSelectors.js
@@ -0,0 +1,4 @@
+export const getMonitors = state => state.monitors.monitors;
+export const getMonitorsLoading = state => state.monitors.loading;
+export const getMonitorsError = state => state.monitors.error;
+
